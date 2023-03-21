a035d4c0b4fbb6402b8d185fa9e90dfe5b8b1562
DarynaHorobei97 <daryna_horobei1@ukr.net>
2023-03-21 20:49:17 +0200

    Commit message

---
+++ src/utils/Car.java
@@ -0,0 +1,23 @@
+package utils;
+
+
+import java.util.Random;
+
+public class Car {
+
+    String color;
+    double weight;
+
+    public void showColor(){
+        int a = 5;
+        int b = 10;
+
+        System.out.println(color);
+    }
+
+
+    public static void main(String[] args) {
+        int _sumOfTwoNumbers = 1;
+    }
+
+}
